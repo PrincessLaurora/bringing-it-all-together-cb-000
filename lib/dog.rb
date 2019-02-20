12f482d5b4536ab121401748b709d18a5895efc7
Laura Felce <crepu83@yahoo.it>
2019-02-20 17:39:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,7 +56,7 @@ attr_accessor :name, :breed, :id
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
-    breed = [1]
+    breed = row[2]
     self.new(id: id, name: name, breed: breed)
   end
 
